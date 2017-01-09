49d3183bc1a20cd945b0e24a40800227ba5391bc
Benc901 <benc901@gmail.com>
2017-01-09 23:14:45 +0200

    EnablePayment SQL

--- src/iBookServer/mysqlConnection.java
+++ src/iBookServer/mysqlConnection.java
@@ -194,6 +194,26 @@ public Object logout(Object obj) {
 		}
 		return returnObj;
 	}	
+	
+	public boolean EnablePayment(ArrayList<Object> details,UserET user){
+	
+		try {
+			Statement  rStmt = con.createStatement();
+			rStmt.executeUpdate("INSERT INTO reader VALUES ("+user.getId()+","
+															 +details.get(0)+","
+															+"\""+details.get(1)+"\""+","
+															+"\""+details.get(2)+"\""+","
+															+"\""+details.get(3)+"\""+","
+															+"\""+details.get(4)+"\""+","
+															+"\""+details.get(5)+"\""+","
+															+0+","+details.get(6));
+			display(" User sent enable payment");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 	public void closeSqlConnection(){
 
 		
